3fc93188f71613970a7499d223e0ffb644eade44
Samsquamptch <m-greening@hotmail.co.uk>
2026-02-11 15:29:53 -0600

    fix: ensured list command is registered on activation of extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,10 @@
 import * as vscode from 'vscode';
 import { registerPropCommands } from './commands/props';
+import { registerListCommands } from './commands/lists';
 
 export function activate(context: vscode.ExtensionContext) {
   registerPropCommands(context);
+  registerListCommands(context);
 }
 
 export function deactivate() {}
